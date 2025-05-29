15e3e4f7ea621ba56d7077218734e20a2f98e8a1
GideonKoenig <gideon.koenig@online.de>
2025-05-29 14:22:44 +0200

    meta: reset

--- src/categorization/types.ts
+++
@@ -1,8 +0,0 @@
-import { Transaction } from "@/readers/csv-reader.ts";
-
-export type Pattern = {
-    name: string;
-    priority: number;
-    matcher: (transaction: Transaction) => boolean;
-    getDetails?: (transaction: Transaction) => string | undefined;
-};
